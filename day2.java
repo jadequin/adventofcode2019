9d5d4126bcaab5d9462efb5f03520c09973ac303
jadequin <53236544+jadequin@users.noreply.github.com>
2019-12-02 23:48:33 +0100

    changed to oo

--- day2.java
+++ day2.java
@@ -1,59 +1,69 @@
-/* Mögliche Verbesserungen:
-/ -Intcodes objektorientiert implementieren
-/ -schlankerer Code durch zusammenfassen?
-*/
+class Intcode {
+    String input;
+    String output;
+    int[] intcodeArr;
 
-int[] translateToInt(String input) {
-    int[] res = new int[input.split(",", -1).length];
-    try {
-        int cnt = 0;
-        for(String val: input.split(",", -1)) {
-            res[cnt] = Integer.parseInt(val);
-            cnt++;
+    Intcode(String input) {
+        this.input = input;
+        intcodeArr = parseToIntArr(input);
+    }
+    private int[] parseToIntArr(String input) {
+        int[] res = new int[input.split(",", -1).length];
+        try {
+            int cnt = 0;
+            for(String val: input.split(",", -1)) {
+                res[cnt] = Integer.parseInt(val);
+                cnt++;
+            }
+        }
+        catch(NumberFormatException nfe) {
+            throw new IllegalArgumentException("please enter integer values, seperated by commas!");
+        }
+        return res;
+    }
+    private void executeOperations() {
+        //assert intcodeArr.length >= 4;
+        //and assert intcodeArr[] contains 99 as a exit possibility
+        int pos = 0;
+        while(intcodeArr[pos] != 99) {
+            int number1 = intcodeArr[intcodeArr[pos + 1]];
+            int number2 = intcodeArr[intcodeArr[pos + 2]];
+
+            if(intcodeArr[pos] == 1)
+                intcodeArr[intcodeArr[pos + 3]] = number1 + number2;
+            else if(intcodeArr[pos] == 2)
+                intcodeArr[intcodeArr[pos + 3]] = number1 * number2;
+            else
+                throw new IllegalArgumentException("illegal operator found: valid operators are 1 for + and 2 for *");
+            pos += 4;
         }
     }
-    catch(NumberFormatException nfe) {
-        throw new IllegalArgumentException("please enter integer values, seperated by commas!");
+    private String arrayToString() {
+        String res = "";
+        for(int i: intcodeArr)
+            res += i + ",";
+        return res.substring(0, res.length() - 1);
     }
-    return res;
-}
-int[] executeOperations(int[] intcode) {
-    //assert intcode.length >= 4;
-    //and assert intcode[] contains 99 as a exit possibility
-
-    int[] res = intcode;
-    int pos = 0;
-    while(intcode[pos] != 99) {
-        int number1 = intcode[intcode[pos + 1]];
-        int number2 = intcode[intcode[pos + 2]];
-
-        if(intcode[pos] == 1)
-            res[intcode[pos + 3]] = number1 + number2;
-        else if(intcode[pos] == 2)
-            res[intcode[pos + 3]] = number1 * number2;
-        else
-            throw new IllegalArgumentException("illegal operator found: valid operators are 1 for + and 2 for *");
-        pos += 4;
+    public String execute() {
+        executeOperations();
+        output = arrayToString();
+        return output;
     }
-    return res;
+    public String toString() {
+        return input;
+    }
 }
-String translateToString(int[] output) {
-    String res = "";
-    for(int i: output)
-        res += i + ",";
-    return res.substring(0, res.length() - 1);
-}
-void test() {
-    String[] programs = {
+
+//test
+String[] programs = {
         "1,0,0,0,99", 
         "2,3,0,3,99", 
         "2,4,4,5,99,0", 
         "1,1,1,4,99,5,6,0,99"};
     
-    for(String s: programs) {
-        int[] intcode = translateToInt(s);
-        int[] executedIntcode = executeOperations(intcode);
-        String result = translateToString(executedIntcode);
-        System.out.println(result);
-    }
+for(String s: programs) {
+    int[] intcodeArr = translateToInt(s);
+    int[] executedIntcode = executeOperations(intcodeArr);
+    String result = translateToString(executedIntcode);
+    System.out.println(result);
 }
