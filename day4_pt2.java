2cbc91f2d99a084a9d55cbe0a787fcfcf1ad7258
jadequin <53236544+jadequin@users.noreply.github.com>
2019-12-04 21:24:18 +0100

    Update day4_pt2.java

--- day4_pt2.java
+++ day4_pt2.java
@@ -1,35 +1,28 @@
-//something isn't working here ... will fix it soon
-
 String input = "265275-781584";
 int lowerBnd = Integer.parseInt(input.split("-")[0]);
 int upperBnd = Integer.parseInt(input.split("-")[1]);
 
-boolean containsTwins;
-int cntTwins;
-boolean adjacentTwins;
+boolean containTwins;
+int cntDoubles;
 int passwordCount = 0;
 
 outer:
 for(int num = lowerBnd; num <= upperBnd; num++) {
     int[] digits = Integer.toString(num).chars().map(c -> c-'0').toArray();
 
-    containsTwins = false;
-    adjacentTwins = false;
-    cntTwins = 0;
+    containTwins = false;
+    cntDoubles = 0;
     for(int i = 1; i < digits.length; i++) {
         if(digits[i] < digits[i - 1])
             continue outer;
-        if(digits[i] == digits[i - 1]) {
-            cntTwins += adjacentTwins? 1: 2;
-            adjacentTwins = true;
-            containsTwins = true;
+        if(digits[i] == digits[i - 1])
+            cntDoubles++;
+        else if(cntDoubles == 1) {
+            containTwins = true;
+            cntDoubles = 0;
         }
-        else if(cntTwins % 2 == 1)
-            continue outer;
-        else {
-            cntTwins = 0;
-            adjacentTwins = false;
-        }
-    }
-    if(containsTwins && cntTwins % 2 == 0) passwordCount++;
+        else
+            cntDoubles = 0;
+    }        
+    if(containTwins || cntDoubles == 1) passwordCount++;
 }
