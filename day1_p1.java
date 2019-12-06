d4d58aaa18afd1890c753fa871b16265db2fc6cf
jadequin <53236544+jadequin@users.noreply.github.com>
2019-12-06 12:25:59 +0100

    Add files via upload

---
+++ day1_p1.java
@@ -0,0 +1,15 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+int fuel(int mass) {
+    return (mass / 3) - 2;
+}
+void part1() {
+    //not working without: 1. copy&paste input line into jshell 2. auskommentieren
+    String input = new String(Files.readAllBytes(Paths.get("day1_input.txt")), "UTF-8");
+    int[] masses = Stream.of(input.split("\r\n")).mapToInt(Integer::parseInt).toArray();
+    int sum = 0;
+    for(int mass: masses)
+        sum += fuel(mass);
+    System.out.println("fuel needed: " + sum);
+}
